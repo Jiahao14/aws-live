6076cac070703c65643040bd550d991b84ca81ff
Jiahao14 <100479590+Jiahao14@users.noreply.github.com>
2023-12-23 22:46:04 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.chhsjsbhv2pq.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "teohjiahao-employee"
+customregion = "us-east-1"
+
