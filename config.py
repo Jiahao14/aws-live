0f5c1ffd20e38385c47f51c4e182ad455235f35f
Jiahao14 <100479590+Jiahao14@users.noreply.github.com>
2023-12-23 22:44:42 +0800

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-customhost = "database-1.chhsjsbhv2pq.us-east-1.rds.amazonaws.com"
-customuser = "aws_user"
-custompass = "Bait3273"
-customdb = "employee"
-custombucket = "teohjiahaoemployee"
-customregion = "us-east-1"
-
